adff3e76d46d2ddb0e8ff0211b88d4110aafdd45
elemae6 <81418010+elemae6@users.noreply.github.com>
2023-10-20 15:19:39 +0900

    Delete Spring Boot directory

--- Spring Boot/firstproject/src/main/java/com/example/firstproject/ioc/Beef.java
+++
@@ -1,7 +0,0 @@
-package com.example.firstproject.ioc;
-
-public class Beef extends Ingredient {
-    public Beef(String name) {
-        super(name);
-    }
-}
--- Spring Boot/firstproject/src/main/java/com/example/firstproject/ioc/Pork.java
+++
@@ -1,7 +0,0 @@
-package com.example.firstproject.ioc;
-
-public class Pork extends Ingredient {
-    public Pork(String name) {
-        super(name);
-    }
-}
